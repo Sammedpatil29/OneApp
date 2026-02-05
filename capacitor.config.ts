900141e6773f555617e0ee47eed90688752820c7
Sammed <sammed.patil29@gmail.com>
2026-02-05 11:03:17 +0530

    splash screen fixed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,12 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.oneapp',
   appName: 'Pintu - Minutes App',
   webDir: 'www',
+  backgroundColor: '#a000e2',
   plugins: {
     FirebaseAuthentication: {
       skipNativeAuth: false,
       providers: ['phone']
     },
     SplashScreen: {
+      backgroundColor: '#a000e2',
       launchShowDuration: 0,
       launchAutoHide: true,
       launchFadeOutDuration: 0,
