9d5a102be64fd29db2289284b3b8365c90b25905
Sammed <sammed.patil29@gmail.com>
2026-05-18 17:31:31 +0530

    minor changes

--- src/app/pages/address-list/address-list.page.ts
+++ src/app/pages/address-list/address-list.page.ts
@@ -114,7 +114,7 @@ addresses: any = []
         this.getAddressList(false)
       })
       this.isToastOpen = true
-      this.toastMessage = "deleted successfully";
+      this.toastMessage = "address deleted successfully";
       setTimeout(()=>{
         this.isToastOpen = false
       },3000)
