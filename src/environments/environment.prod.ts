c81df02c9d6d66f1e06056fa0e7673316517c9f1
Sammed <sammed.patil29@gmail.com>
2026-09-13 11:38:10 +0530

    admob integrated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,13 @@ export const environment = {
   socketUrl: 'https://pintu-api.democompany.in.net',
   googleMapsApiKey: 'AIzaSyA85HFedGjgP12MG_dvR-MVgooWTcJNIb0',
   razorpayKeyId: 'rzp_live_p6MXH1oq4BBYPk',
-  version: '0.0.3'
+  version: '0.0.3',
+  admob: {
+    // When real AdMob IDs are created, replace these test IDs and set isTesting to false
+    bannerAdUnitId: 'ca-app-pub-3940256099942544/6300978111',
+    interstitialAdUnitId: 'ca-app-pub-3940256099942544/1033173712',
+    rewardedAdUnitId: 'ca-app-pub-3940256099942544/5224354917',
+    isTesting: true
+  }
 };
+
